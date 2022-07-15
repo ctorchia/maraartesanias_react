4d707b38791d30c42a4fc4c64cfcc64e22a91632
ctorchia <cristian_torchia@yahoo.com.ar>
2022-07-15 17:34:27 -0300

    Ajustes en Form del Carrito

--- src/components/CartForm/CartForm.jsx
+++ src/components/CartForm/CartForm.jsx
@@ -4,19 +4,19 @@ import { Button } from 'react-bootstrap'
 const CartForm = ({ generateOrder }) => {
 
     return (
-        <div>
-            <h3>Datos del Cliente:</h3>
+        <div className="col-md-12">
+            <h3 className=" text-center m-4">Datos del Cliente:</h3>
             <form className="row g-3 form-control-lg mb-5" onSubmit={generateOrder}>
-                <div className="col-md-3">
+                <div className="col-md-4">
                     <label htmlFor="name" className="form-label">Nombre:</label>
                     <input type="text" className="form-control" id="name" required />
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-5">
                     <label htmlFor="email" className="form-label">Email:</label>
                     <input type="email" className="form-control" id="email" required />
                 </div>
                 <div className="col-md-3">
-                    <label htmlFor="phone" className="form-label">Telefono:</label>
+                    <label htmlFor="phone" className="form-label">Teléfono:</label>
                     <input type="text" className="form-control" id="phone" required />
                 </div>
                 <div className="col-12">
